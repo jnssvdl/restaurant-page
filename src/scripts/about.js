db881acfba5168f9a4c19f760b0f266ab94e94f9
Joeniesus Vidal <132261842+jnssvdl@users.noreply.github.com>
2024-02-15 00:06:05 +0800

    refactored about module

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -6,8 +6,12 @@ function about() {
     container.setAttribute('id', 'about-container');
 
     const description = document.createElement('p');
-    description.innerHTML = 'This webpage serves as a project assignment submission for the <a href="https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page">Restaurant Page of The Odin Project\'s JavaScript course</a>. The purpose of the creation this webpage is to exercise writing organized code utilizing ES6 modules and Webpack. Please note that this website is not real and was created solely for educational purposes. All images used on the webpage are not mine and are credited to their respective owners.';
+    description.innerHTML = 'This webpage serves as a project assignment submission for the <a href="https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page">Restaurant Page of The Odin Project\'s JavaScript course</a>. The purpose of the creation this webpage is to exercise writing organized code utilizing ES6 modules and Webpack.';
     container.appendChild(description);
+
+    const note = document.createElement('p');
+    note.textContent = ' Please note that this website is not real and was created solely for educational purposes. All images used on the webpage are not mine and are credited to their respective owners.';
+    container.appendChild(note);
 
     const link = document.createElement('a');
     link.setAttribute('id', 'source-code');
